ead71f620653658503eb3dc5ba5215b9a4648cad
Kian Davoudi-Rad <davoudi.rk@gmail.com>
2017-08-04 16:38:41 -0700

    adding inital files

---
+++ NNDWebAPI/NNDWebAPI/NNDWebAPI.h
@@ -0,0 +1,19 @@
+//
+//  NNDWebAPI.h
+//  NNDWebAPI
+//
+//  Created by Kian Davoudi-Rad on 2017-08-04.
+//  Copyright © 2017 Kian Davoudi-Rad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NNDWebAPI.
+FOUNDATION_EXPORT double NNDWebAPIVersionNumber;
+
+//! Project version string for NNDWebAPI.
+FOUNDATION_EXPORT const unsigned char NNDWebAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NNDWebAPI/PublicHeader.h>
+
+
